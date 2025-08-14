26efc32f9281baf8a704bce208d324cd0503bf58
DEVELOPER-BERA <berabruce9@gmail.com>
2025-08-14 14:57:45 +0300

    Create colors.js

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+// src/constants/colors.js
+export const colors = {
+  primary: '#075e54',
+  secondary: '#128c7e',
+  accent: '#25d366',
+  white: '#ffffff',
+  lightGray: '#f5f5f5',
+  gray: '#cccccc',
+  darkGray: '#666666',
+  black: '#000000',
+  error: '#ff4444',
+};
