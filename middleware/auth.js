1107c6d47888143add2c171a2aa881353aedc80c
DEVELOPER-BERA <berabruce9@gmail.com>
2025-08-16 14:56:02 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// Protect routes
 exports.protect = async (req, res, next) => {
   let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.token) {
-    token = req.cookies.token;
   }
 
   if (!token) {
@@ -22,7 +16,6 @@ exports.protect = async (req, res, next) => {
   }
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
     next();
@@ -32,17 +25,4 @@ exports.protect = async (req, res, next) => {
       error: 'Not authorized to access this route'
     });
   }
-};
-
-// Grant access to specific roles
-exports.authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({
-        success: false,
-        error: `User role ${req.user.role} is not authorized to access this route`
-      });
-    }
-    next();
-  };
 };
