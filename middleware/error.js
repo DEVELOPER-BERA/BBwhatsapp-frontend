82a71797a1ebafc82e0d06afcf76260aabab0c76
DEVELOPER-BERA <berabruce9@gmail.com>
2025-08-16 13:13:33 +0300

    Create error.js

---
+++ middleware/error.js
@@ -0,0 +1,18 @@
+exports.errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  res.status(statusCode).json({
+    success: false,
+    error: message
+  });
+};
+
+exports.notFound = (req, res, next) => {
+  res.status(404).json({
+    success: false,
+    error: 'Not found'
+  });
+};
