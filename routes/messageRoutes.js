62e06b583894fc6ab69325714a86425f9e4cf165
DEVELOPER-BERA <berabruce9@gmail.com>
2025-08-17 20:56:36 +0300

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/auth');
+const auth = require('../middleware/auth');
 const messageController = require('../controllers/messageController');
 
-// Apply protection middleware to all routes
-router.use(protect);
-
-// Get messages between users
-router.get('/:userId', messageController.getMessages);
-
-// Create new message
-router.post('/', messageController.createMessage);
-
-// Update message status
-router.put('/:id/status', messageController.updateMessageStatus);
+router.get('/:userId', auth, messageController.getMessages);
 
 module.exports = router;
