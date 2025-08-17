af34d3090cac33f7c81be69fb16ecc14d95c44b2
DEVELOPER-BERA <berabruce9@gmail.com>
2025-08-17 20:42:46 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,22 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-exports.protect = async (req, res, next) => {
-  let token;
-
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
-  }
-
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      error: 'Not authorized to access this route'
-    });
-  }
-
+const auth = async (req, res, next) => {
   try {
+    const token = req.header('Authorization').replace('Bearer ', '');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+    const user = await User.findOne({ _id: decoded._id });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    req.token = token;
     next();
-  } catch (err) {
-    return res.status(401).json({
-      success: false,
-      error: 'Not authorized to access this route'
-    });
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate.' });
   }
 };
+
+module.exports = auth;
