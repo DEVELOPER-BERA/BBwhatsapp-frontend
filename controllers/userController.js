777649c21bafa4af31dfd96ab5d8e3ad7831c855
DEVELOPER-BERA <berabruce9@gmail.com>
2025-08-17 20:53:29 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,10 @@
 const User = require('../models/User');
 
-// @desc    Get all users
-// @route   GET /api/users
-// @access  Private
-exports.getUsers = async (req, res, next) => {
+exports.getAllUsers = async (req, res) => {
   try {
-    // Exclude the current user from the list
-    const users = await User.find({ _id: { $ne: req.user.id } })
-      .select('-password')
-      .sort({ online: -1, name: 1 });
-
-    res.status(200).json({
-      success: true,
-      count: users.length,
-      data: users
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-// @desc    Get current user profile
-// @route   GET /api/users/me
-// @access  Private
-exports.getMe = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-
-    res.status(200).json({
-      success: true,
-      data: user
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-// @desc    Update user profile
-// @route   PUT /api/users/me
-// @access  Private
-exports.updateProfile = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.user.id, req.body, {
-      new: true,
-      runValidators: true
-    }).select('-password');
-
-    res.status(200).json({
-      success: true,
-      data: user
-    });
-  } catch (err) {
-    next(err);
+    const users = await User.find({ _id: { $ne: req.user._id } }).select('-password');
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
   }
 };
